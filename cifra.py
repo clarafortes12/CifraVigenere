143acb897c7ba598c1e813c79ee2aa675e2f0ca3
Thiago Cardoso <thiagocpais@hotmail.com>
2023-04-28 16:05:23 -0300

    Adição das funções de cifrar e decifrar com duas funções: transformando tudo para upper case e mantendo case original

--- cifra.py
+++ cifra.py
@@ -1,18 +1,61 @@
 class Cifra:
-    def __init__(self, file):
-        self.arquivo = file
-        self.senha = ""
-        self.textoOriginal = ""
+    def __init__(self):#, file):
+        #self.arquivo = file
+        self.senha = "senhaBoa"
+        self.textoOriginal = "teste DE mensagem" #uftuf ef nfotbhfn
         self.textoCifrado = ""
     
     def getArquivo(self):
-        print("pega arquivo");
+        print("pega arquivo")
     
     def newChave(self):
-        print("criar a chave e salvar com a de salvar do chaves.py");
+        print("criar a chave e salvar com a de salvar do chaves.py")
     
     def cifrar(self):
-        print("fazer a cifra do testo");
+        print("fazer a cifra do teste")
+        #Colocar texto em textoOriginal
+        self.textoCifrado = ""
+        textoRef = self.textoOriginal.upper()
+        self.senha = self.senha.upper()
+        refA = ord('A')
+        i = 0
+        for letra in textoRef:
+            increm = ord(self.senha[i%len(self.senha)]) - refA
+            if (ord(letra) >= ord('A')) and (ord(letra) <= ord('Z')):
+                cod = ((ord(letra) - refA) + increm) % 26
+                cod += refA
+            else:
+                cod = ord(letra)
+            self.textoCifrado += chr(cod)
+            i += 1
+            
+    def cifrarCase(self):
+        #Colocar texto em textoOriginal
+        self.textoCifrado = ""
+        self.senha = self.senha.upper()
+        refA = ord('A')
+        refa = ord('a')
+        i = 0
+        for letra in self.textoOriginal:
+            increm = ord(self.senha[i%len(self.senha)]) - refA
+            if (ord(letra) >= ord('A')) and (ord(letra) <= ord('Z')):
+                cod = ((ord(letra) - refA) + increm) % 26
+                cod += refA
+            elif (ord(letra) >= ord('a')) and (ord(letra) <= ord('z')):
+                cod = ((ord(letra) - refa) + increm) % 26
+                cod += refa
+            else:
+                cod = ord(letra)
+            self.textoCifrado += chr(cod)
+            i += 1
     
     def save(self):
-        print("salvar o arquivo Cifrado");
+        print("salvar o arquivo Cifrado")
+        
+    
+obj = Cifra()
+
+obj.cifrar()
+print(obj.textoCifrado)
+obj.cifrarCase()
+print(obj.textoCifrado)
--- decifra.py
+++ decifra.py
@@ -1,16 +1,59 @@
 class Decifra:
-    def __init__(self, file):
-        self.arquivo = file
-        self.senha = ""
-        self.textoOriginal = ""
+    def __init__(self):#, file):
+        #self.arquivo = file
+        self.senha = "senhaBoa"
+        self.textoOriginal = "lifae RE qrusbuee"
         self.textoCifrado = ""
     
     def getArquivo():
-        print("pega arquivo cifrado");
-        print("pega arquivo chave");
+        print("pega arquivo cifrado")
+        print("pega arquivo chave")
 
     def decifrar(self):
-        print("fazer o decifra do texto");
+        print("fazer o decifra do texto")
+        #Colocar texto em textoOriginal
+        self.textoCifrado = ""
+        textoRef = self.textoOriginal.upper()
+        self.senha = self.senha.upper()
+        refA = ord('A')
+        i = 0
+        for letra in textoRef:
+            increm = ord(self.senha[i%len(self.senha)]) - refA
+            if (ord(letra) >= ord('A')) and (ord(letra) <= ord('Z')):
+                cod = ((ord(letra) - refA) - increm) % 26
+                cod += refA
+            else:
+                cod = ord(letra)
+            self.textoCifrado += chr(cod)
+            i += 1
+            
+    def decifrarCase(self):
+        #Colocar texto em textoOriginal
+        self.textoCifrado = ""
+        self.senha = self.senha.upper()
+        refA = ord('A')
+        refa = ord('a')
+        i = 0
+        for letra in self.textoOriginal:
+            increm = ord(self.senha[i%len(self.senha)]) - refA
+            if (ord(letra) >= ord('A')) and (ord(letra) <= ord('Z')):
+                cod = ((ord(letra) - refA) - increm) % 26
+                cod += refA
+            elif (ord(letra) >= ord('a')) and (ord(letra) <= ord('z')):
+                cod = ((ord(letra) - refa) - increm) % 26
+                cod += refa
+            else:
+                cod = ord(letra)
+            self.textoCifrado += chr(cod)
+            i += 1
     
     def save(self):
-        print("salvar o arquivo original");
+        print("salvar o arquivo original")
+        
+        
+obj = Decifra()
+
+obj.decifrar()
+print(obj.textoCifrado)
+obj.decifrarCase()
+print(obj.textoCifrado)
